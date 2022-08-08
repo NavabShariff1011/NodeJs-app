5cfadd8a0bbf94b79561a6f18e23d30fa099ced3
root <root@ip-172-31-6-221.ap-south-1.compute.internal>
2022-08-08 14:25:43 +0000

    sonar errors

--- src/sonar-project.js
+++ src/sonar-project.js
@@ -8,7 +8,7 @@ sonarqubeScanner({
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
-            'sonar.sources': 'src/**',
+            'sonar.sources': '.',
 	  //'sonar.tests': 'specs',
           //'sonar.inclusions' : 'src/**'
        },
