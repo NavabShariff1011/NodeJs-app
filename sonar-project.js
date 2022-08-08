64b113ec45f605b66fd42c3417b6252d0fd2d22b
root <root@ip-172-31-6-221.ap-south-1.compute.internal>
2022-08-08 14:56:07 +0000

    sjdj

--- sonar-project.js
+++ sonar-project.js
@@ -1,13 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-	serverUrl: 'http://52.66.210.76:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'admin',
-	    'sonar.password': 'navabshariff',
+	    //'sonar.login': 'admin',
+	    //'sonar.password': 'navabshariff',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
