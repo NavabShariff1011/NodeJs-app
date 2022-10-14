de03177215779453d66b619c47aa48856928284c
Tarang Ambaliya <taranga@webelight.co.in>
2022-10-14 13:01:06 +0530

    ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps : [{
+    name   : "shariff",
+    script : "npm run start",
+    env: {
+	    NODE_ENV: "development",
+            PORT: "5000"	    
+      },
+  }],
+};
